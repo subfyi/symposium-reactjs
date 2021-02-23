769076090f489cdc8a67fe170561307d6d84d791
akinayturan <akinayturan@gmail.com>
2021-02-23 14:43:21 +0300

    edt

--- src/themes/dark.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import 'bootswatch/dist/darkly/bootstrap.css';
-
-const Theme = () => (<React.Fragment></React.Fragment>);
-
-export default Theme;
--- src/themes/light.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Theme = () => (<React.Fragment></React.Fragment>);
-
-export default Theme;
