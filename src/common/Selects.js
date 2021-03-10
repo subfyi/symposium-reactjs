5252a1d57d8a6bf95fb919ec8264651ca0165474
akinayturan <akinayturan@gmail.com>
2021-03-10 11:35:40 +0300

    edt bck

--- src/common/Selects.js
+++ src/common/Selects.js
@@ -1,8 +1,7 @@
 import React, {useCallback} from 'react';
 import {ApiSelect} from "react-admin-base-adminkit";
-import {useAuth} from "react-admin-base";
 
-export function AuthorSelects(props) {
+export function AuthorSelect(props) {
     const onCreateOption = useCallback(async function(email) {
         return { email };
     }, []);
@@ -11,28 +10,6 @@ export function AuthorSelects(props) {
         { row => <>{row.email} (<b>{row.first_name} {row.last_name}</b>)</> }
     </ApiSelect>;
 }
-
-
-
-export function AuthorSelect(props) {
-    const [ api ] = useAuth();
-
-    const url = "/api/author";
-
-    const onCreateOption = useCallback(async function(input) {
-        const data = await api.tokenized.put(url, { value: input });
-        return data.data;
-    }, [api, url]);
-
-    return <ApiSelect
-        idKey="email"
-        nameKey="email"
-        url={url}
-        onCreateOption={onCreateOption}
-        {...props}
-    />;
-}
-
 
 export function ParameterSelect(props) {
     const { ptur, type } = props;
