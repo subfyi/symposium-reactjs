b24db1ba655a3434cfb915aebc63d888a2e66b14
akinayturan <akinayturan@gmail.com>
2021-03-11 15:40:13 +0300

    edt

--- src/MenuSidebar.js
+++ src/MenuSidebar.js
@@ -17,6 +17,13 @@ export default injectIntl(function MenuSidebar() {
             <Menu title={"Poster Presentations"} to="/presentation/poster" icon="fas fa-file-powerpoint"/>
         </MenuGroup>
         }
+
+
+        {user.role >= 8 &&
+        <MenuGroup title="Other">
+            <Menu title={"Author"} to="/author" icon="fas fa-minus"/>
+        </MenuGroup>
+        }
         <MenuGroup title="Settings">
             {user.role >= 8 && <Menu title="Users" to="/user" icon="fas fa-users"/>}
             {user.role >= 8 && <Menu title={"Roles"} to="/roles" icon="fas fa-magic"/>}
