bf03f2ad7cec41b6cc52f5b19cb0649db8209265
akinayturan <akinayturan@gmail.com>
2021-05-22 20:38:23 +0300

    super ^^

--- src/pages/Presentations/Detay.js
+++ src/pages/Presentations/Detay.js
@@ -1,87 +1,80 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {Button, ButtonGroup, Card, CardBody, CardHeader, Table} from 'reactstrap';
 import Moment from 'react-moment';
 import {CKEditor} from "react-admin-base-ckeditor";
 import {SingleFilePicker} from "react-admin-base-adminkit";
+import {useUser} from "../../Components/UserProvider";
 
-export default class Detay extends Component {
-    state = {};
+export default function Detay({value, onSave, onChange}) {
+    const [editingRow, setEditingRow] = useState();
+    const user = useUser();
 
-    render() {
-        if (!this.state.user)
-            return null;
+    return <Card>
+        <CardHeader>
+            <b> Questions & Answers</b>
+        </CardHeader>
+        <CardBody>
+            <Table striped bordered>
+                <thead>
+                <tr>
+                    <th style={{width: '30%'}}>Creator</th>
+                    <th style={{width: '20%'}}>Created At</th>
+                    <th style={{width: '20%'}}>Updated At</th>
+                    <th style={{width: '20%'}}>Past Time</th>
+                    <th style={{width: '10%'}}>Operation</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    (value || []).map(row => <>
+                        <tr>
+                            <td>{(row.user && row.user.name + " " + row.user.surname) || 'You'}</td>
+                            <td>{row.created_at || "Now"}</td>
+                            <td>{row.updated_at || "Now"}</td>
+                            <td><Moment fromNow ago>{row.created_at}</Moment></td>
+                            <td className="d-flex flex-column">
+                                {editingRow === row ? <ButtonGroup>
+                                    <Button color="primary" onClick={a => {
+                                        row.updated_at = null;
+                                        setEditingRow(null);
+                                        onSave();
+                                    }}>Edit</Button>
+                                </ButtonGroup> : <ButtonGroup>
+                                    {((row.user && row.user.id == user.id) || user.role >= 8) && <Button color="warning" size="sm" onClick={a => setEditingRow(row)}>Edit</Button>}
+                                </ButtonGroup>}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan="5">
+                                {(row.dosyalar || []).map(dosya => <div className="mt-1 mb-1"><SingleFilePicker value={dosya.dosya} disabled noThumb/></div>)}
 
+                                {editingRow === row ? <>
+                                    <CKEditor
+                                        value={(row.initialData || (row.initialData = row.isicerik)) || ""}
+                                        onChange={a => {
+                                            row.isicerik = a;
+                                        }}
+                                    />
+                                </> : <div className="icerik-alani" dangerouslySetInnerHTML={{__html: row.isicerik}}/>}
+                            </td>
+                        </tr>
+                    </>)
+                }
+                </tbody>
+            </Table>
+            <Button className="col-md-12" color="primary" onClick={a => {
+                if (editingRow) {
+                    editingRow.updated_at = null;
+                    setEditingRow(null);
+                    onSave();
+                    return;
+                }
 
-        return (  <Card>
-            <CardHeader title="Questions & Answers" />
-                <CardBody>
-                <Table striped bordered>
-                    <thead>
-                    <tr>
-                        <th style={{ width: '30%' }}>Creator</th>
-                        <th style={{ width: '20%' }}>Created At</th>
-                        <th style={{ width: '20%' }}>Updated At</th>
-                        <th style={{ width: '20%' }}>Past Time</th>
-                        <th style={{ width: '10%' }}>Operation</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        (this.props.value || []).map(row => <>
-                            <tr>
-                                <td>{(row.user && row.user.name + " " +row.user.surname) || 'You'}</td>
-                                <td>{row.created_at || "Now"}</td>
-                                <td>{row.updated_at || "Now"}</td>
-                                <td><Moment fromNow ago>{row.created_at}</Moment></td>
-                                <td className="d-flex flex-column">
-                                    {this.state.editingRow === row ? <ButtonGroup>
-                                        <Button color="primary" onClick={a => {
-                                            row.updated_at = null;
-                                            this.setState({editingRow: null});
-                                            this.props.onSave();
-                                        }}>Edit</Button>
-                                    </ButtonGroup> : <ButtonGroup>
-                                        { (row.user.id == this.state.user.id || this.state.user.role >= 8) && <Button color="warning" size="sm"  onClick={a => this.setState({editingRow: row})}>Edit</Button>}
-                                    </ButtonGroup>}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colSpan="5">
-                                    {(row.dosyalar || []).map(dosya => <div className="mt-1 mb-1"><SingleFilePicker value={dosya.dosya} disabled noThumb /></div>)}
+                var obj = {};
 
-                                    {this.state.editingRow === row ? <>
-                                        <CKEditor
-                                            editor={ClassicEditor}
-                                            data={(row.initialData || (row.initialData = row.isicerik)) || ""}
-                                            onChange={(event, editor) => {
-                                                const data = editor.getData();
-                                                row.isicerik = data;
-                                            }}
-                                        />
-                                    </> : <div className="icerik-alani" dangerouslySetInnerHTML={{__html: row.isicerik}}/>}
-                                </td>
-                            </tr>
-                        </>)
-                    }
-                    </tbody>
-                </Table>
-                <Button className="col-md-12" color="primary" onClick={a => {
-                    if(this.state.editingRow) {
-                        this.state.editingRow.updated_at = null;
-                        this.setState({editingRow: null});
-                        this.props.onSave();
-                        return ;
-                    }
-
-                    var obj = {
-                        user: {name: 'You'}
-                    };
-
-                    this.props.onChange((this.props.value || []).concat([obj]));
-                    this.setState({editingRow: obj});
-                }}>{ this.state.editingRow ? 'Save' : 'Add New' }</Button>
-                </CardBody>
-            </Card>
-        );
-    }
+                onChange((value || []).concat([obj]));
+                setEditingRow(obj);
+            }}>{editingRow ? 'Save' : 'Add New'}</Button>
+        </CardBody>
+    </Card>
 }
--- src/pages/Presentations/Watch.js
+++ src/pages/Presentations/Watch.js
@@ -5,6 +5,8 @@ import {useUser} from "../../Components/UserProvider";
 import {useEntity} from "react-admin-base";
 import {Redirect} from "react-router-dom";
 import {Breadcrumb, EntityEditor} from "react-admin-base-adminkit";
+import AspectRatio from "react-aspect-ratio";
+import Detay from "./Detay";
 
 export default function Watch(props) {
     const id = props.match.params.id;
@@ -20,7 +22,7 @@ function WatchSlug({match}) {
 
     const user = useUser();
     const entity = useEntity('/api/submission', id);
-    const [data, setData] = entity;
+    const [data, setData, save] = entity;
 
     return <Breadcrumb
         title={data.en_title}
@@ -39,10 +41,22 @@ function WatchSlug({match}) {
     >
         {data && data.id && +id !== +data.id && <Redirect to={"/submission/" + data.id + "/edit"}/>}
         <EntityEditor entity={entity}>
+            {data.video &&  <Card>
+                <CardHeader>
+                    Presentations
+                </CardHeader>
+
+                <AspectRatio ratio="16/9" style={{width: '100%'}}>
+                    <iframe style={{width: '100%'}} frameBorder="0" src={`https://drive.google.com/file/d/${data.video.g_dosyaismi}/preview`} allowFullScreen/>
+                </AspectRatio>
+
+            </Card>}
             <Card>
-                <CardHeader
-                    title={"Title: " + data.en_title}/>
+                <CardHeader>
+                    Title:{data.en_title}
+                </CardHeader>
                 <CardBody>
+
                     <p>
                         {data.authors && data.authors.map((author, index) =>
                             <>{author.first_name} {author.last_name}
@@ -61,6 +75,13 @@ function WatchSlug({match}) {
                     </p>
                 </CardBody>
             </Card>
+                <Detay
+                    value={data.icerikler}
+                    onChange={val => setData({icerikler: val})}
+                    onSave={save}
+                />
+
+
         </EntityEditor>
     </Breadcrumb>;
 }
